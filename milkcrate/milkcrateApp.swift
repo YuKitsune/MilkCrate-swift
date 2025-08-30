779578d8d3001a91f7b2723ddc6b672d73e9a925
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2025-08-30 12:14:07 +1000

    Initial Commit

---
+++ milkcrate/milkcrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  milkcrateApp.swift
+//  milkcrate
+//
+//  Created by Eoin Motherway on 30/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct milkcrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
